e3592a067ede0fea686d5229d980a94672e80652
Amor Ibrahim <127536609+AmorIbrahim@users.noreply.github.com>
2025-07-08 21:34:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        QuantumBookstore store = new QuantumBookstore();
+        Bookstore store = new Bookstore();
 
-        store.addBook(new PaperBook("123", "Java Programming", 2025, 50, 10));
+        store.addBook(new PaperBook("123", "Algo", 2025, 50, 10));
         store.addBook(new EBook("456", "Python Basics", 2020, 80, "PDF"));
         store.addBook(new Demobook("789", "C++", 2004));
 
@@ -14,16 +14,16 @@ public class Main {
         try {
 
             double total = store.buyBook("456", 1, "amorebrahim58@gmail.com");
-            System.out.println("Quantum book store: Paid amount: $" + total);
+            System.out.println("Paid amount: $" + total);
 
             total = store.buyBook("123", 5, "19 Salah Hamed St, Cairo");
-            System.out.println("Quantum book store: Paid amount: $" + total);
+            System.out.println("Paid amount: $" + total);
 
             store.buyBook("789", 1, "amorebrahim58@gmail.com");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("Quantum book store: Current inventory size: " + store.getInventorySize());
+        System.out.println("Current inventory size: " + store.getInventorySize());
     }
 }
