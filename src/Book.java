112e45b20657fed86e4054f39e74532780960b9a
Amor Ibrahim <127536609+AmorIbrahim@users.noreply.github.com>
2025-07-08 21:17:42 +0300

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -8,14 +8,12 @@ import java.util.stream.Collectors;
 public abstract class Book {
     protected String isbn;
     protected String title;
-    protected String author;
     protected int year;
     protected double price;
 
     public Book(String isbn, String title, String author, int year, double price) {
         this.isbn = isbn;
         this.title = title;
-        this.author = author;
         this.year = year;
         this.price = price;
     }
